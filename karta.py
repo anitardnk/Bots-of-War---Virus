4b277014ac589c55b86c6228af8b335f8a9d5e86
anna-czer <128543773+anna-czer@users.noreply.github.com>
2023-03-28 21:08:40 +0200

    Update karta.py

--- karta.py
+++ karta.py
@@ -1,11 +1,7 @@
 import random
-from main import talia
+#from main import talia
 class Karta:
-    def __init__(self):
-        self.talia = talia
+    def __init__(self, kolor, funkcja):
+        self.kolor = kolor
+        self.funkcja = funkcja
     
-    def losuj_karte(self):
-        wylosowana_karta = random.choice(self.talia)
-        print("Wylosowana karta to:", wylosowana_karta)
-
-Karta().losuj_karte()
