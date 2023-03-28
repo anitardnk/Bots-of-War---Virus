4cf17193352382da5b88b4496276109ee3a84aac
vverka <128545659+vverka@users.noreply.github.com>
2023-03-28 23:56:18 +0200

    Update gracz.py

--- gracz.py
+++ gracz.py
@@ -8,10 +8,4 @@ class Gracz:
 
     def wyloz_karte(self):
         print("Wykladam karte!")
-
-def main():
-    gracz1 = Gracz(1, "Gracz1")
-    gracz1.wymien_karte()
-    gracz1.wyloz_karte()
-
-main()
+        
