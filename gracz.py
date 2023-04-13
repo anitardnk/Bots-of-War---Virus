aa516610b14c9f6c0bf6a2a65e1a76bf62fb2ac2
vverka <128545659+vverka@users.noreply.github.com>
2023-04-13 20:31:42 +0200

    Update gracz.py

--- gracz.py
+++ gracz.py
@@ -1,11 +1,18 @@
+
+
 class Gracz:
-    def __init__(self, id, nazwa):
-        self.id = id
+    def __init__(self, gracz_id, nazwa):
+        self.gracz_id = gracz_id
         self.nazwa = nazwa
 
-    def wymien_karte(self):
-        print("Wymineniam karte!")
+    def wymien_karte(self, gracz_id):
+        from interfejsy import InterfejsPlanszy
+        karta=input("Wybierz karte do wymiany: ")
+        return InterfejsPlanszy.wymien_karte(karta, gracz_id)
 
-    def wyloz_karte(self):
-        print("Wykladam karte!")
+    def wyloz_karte(self, gracz_id):
+        from interfejsy import InterfejsPlanszy
+        karta=input("Wybierz karte do wylozenia: ")
+        return InterfejsPlanszy.wyloz_karte(karta, gracz_id)
         
+
