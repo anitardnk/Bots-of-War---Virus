ea40c2a9659577be8510a32c1ceac57d3b245987
vverka <128545659+vverka@users.noreply.github.com>
2023-04-13 20:31:14 +0200

    Update interfejsy.py

--- interfejsy.py
+++ interfejsy.py
@@ -1,7 +1,24 @@
-
+from plansza import Plansza
+from karta import Karta
+from gracz import Gracz
 
 class InterfejsPlanszy:
 
     #nie wiem czy te metody nie powinny byc statyczne
     def sprawdz_czy_ruch_legalny(self, gracz_id):
         pass
+    @staticmethod
+    def wymien_karte(karta, gracz_id):
+        if karta in Plansza.karty_na_reku[gracz_id]:
+            Plansza.karty_na_reku[gracz_id].remove(karta)
+            Plansza.daj_karty_graczowi(1, gracz_id)
+            return True
+        else:
+            return False
+    @staticmethod
+    def wyloz_karte(karta, gracz_id):
+        if karta in Plansza.karty_na_reku[gracz_id]:
+            Plansza.poloz_karte(karta, gracz_id)
+            return True
+        else:
+            return False
