9ccbd6db02e93f3536b72b9779aae118e01b9aa5
Anna Rudenko <259075@student.pwr.edu.pl>
2023-04-18 19:07:10 +0200

    finished interfaces

--- interfejsy.py
+++ interfejsy.py
@@ -1,8 +1,12 @@
 class InterfejsKarty:
     def __init__(self, kolor, funkcja):
         pass
-    
-class InterfejsInterfejsuPlanszy:
+
+
+from posrednik import PosrednikGraczPlansza
+
+
+class InterfejsPosrednikGraczPlansza`:
     def sprawdz_czy_ruch_legalny(self, gracz_id):
         pass
     @staticmethod
@@ -12,23 +16,27 @@ class InterfejsInterfejsuPlanszy:
     def wyloz_karte(karta, gracz_id): 
         pass
 
-class InterfejsPlanszy(InterfejsInterfejsuPlanszy):
+class InterfejsPlanszy:
 
-    #nie wiem czy te metody nie powinny byc statyczne
-    def sprawdz_czy_ruch_legalny(self, gracz_id):
+    def __init__(self, talia):
         pass
-    @staticmethod
-    def wymien_karte(karta, gracz_id):
-        if karta in Plansza.karty_na_reku[gracz_id]:
-            Plansza.karty_na_reku[gracz_id].remove(karta)
-            Plansza.daj_karty_graczowi(1, gracz_id)
-            return True
-        else:
-            return False
-    @staticmethod
-    def wyloz_karte(karta, gracz_id):
-        if karta in Plansza.karty_na_reku[gracz_id]:
-            Plansza.poloz_karte(karta, gracz_id)
-            return True
-        else:
-            return False
+
+    def rozdaj_karty(self, ilosc_graczy):
+        pass
+
+    def daj_karty_graczowi(self, ilosc_kart, gracz_id):
+       pass
+
+    def poloz_karte(self, indeks_karty, gracz_id):
+       pass
+
+class InterfejsGracz:
+    def __init__(self, gracz_id, nazwa):
+        pass
+
+    def wymien_karte(self, gracz_id):
+        pass
+
+    def wyloz_karte(self, gracz_id):
+        pass
+
