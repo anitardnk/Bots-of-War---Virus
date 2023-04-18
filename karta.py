9a2397c7769c7ff55cb373940691cd5df10815f4
anna-czer <128543773+anna-czer@users.noreply.github.com>
2023-04-18 16:02:27 +0200

    Update karta.py

--- karta.py
+++ karta.py
@@ -1,6 +1,8 @@
 import random
+from interfejsy import InterfejsKarty
+
 #from main import talia
-class Karta:
+class Karta(InterfejsKarty):
     def __init__(self, kolor, funkcja):
         self.kolor = kolor
         self.funkcja = funkcja
