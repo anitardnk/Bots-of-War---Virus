56ce8d94b20cb5a387d8e772eb4c3f53f3411125
vverka <128545659+vverka@users.noreply.github.com>
2023-05-08 20:51:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,87 +7,92 @@ from gracz import Gracz
 from boty import BotRandomowy
 
 
-karta1 = Karta("zielony", "wirus")
-karta2 = Karta("zielony", "wirus")
-karta3 = Karta("zielony", "wirus")
-karta4 = Karta("zielony", "wirus")
-
+karta1 = Karta("zielony", "organ")
+karta2 = Karta("zielony", "organ")
+karta3 = Karta("zielony", "organ")
+karta4 = Karta("zielony", "organ")
 karta5 = Karta("zielony", "organ")
-karta6 = Karta("zielony", "organ")
-karta7 = Karta("zielony", "organ")
-karta8 = Karta("zielony", "organ")
-karta9 = Karta("zielony", "organ")
 
-karta10 = Karta("zielony", "lek")
-karta11 = Karta("zielony", "lek")
-karta12 = Karta("zielony", "lek")
-karta13 = Karta("zielony", "lek")
+karta6 = Karta("czerwony", "organ")
+karta7 = Karta("czerwony", "organ")
+karta8 = Karta("czerwony", "organ")
+karta9 = Karta("czerwony", "organ")
+karta10 = Karta("czerwony", "organ")
 
-karta14 = Karta("czerwony", "wirus")
-karta15 = Karta("czerwony", "wirus")
-karta16 = Karta("czerwony", "wirus")
-karta17 = Karta("czerwony", "wirus")
+karta11 = Karta("niebieski", "organ")
+karta12 = Karta("niebieski", "organ")
+karta13 = Karta("niebieski", "organ")
+karta14 = Karta("niebieski", "organ")
+karta15 = Karta("niebieski", "organ")
 
-karta18 = Karta("czerwony", "organ")
-karta19 = Karta("czerwony", "organ")
-karta20 = Karta("czerwony", "organ")
-karta21 = Karta("czerwony", "organ")
-karta22 = Karta("czerwony", "organ")
+karta16 = Karta("żółty", "organ")
+karta17 = Karta("żółty", "organ")
+karta18 = Karta("żółty", "organ")
+karta19 = Karta("żółty", "organ")
+karta20 = Karta("żółty", "organ")
 
-karta23 = Karta("czerwony", "lek")
-karta24 = Karta("czerwony", "lek")
-karta25 = Karta("czerwony", "lek")
+karta21 = Karta("wielokolorowy", "organ")
+'''
+karta22 = Karta("zielony", "lek")
+karta23 = Karta("zielony", "lek")
+karta24 = Karta("zielony", "lek")
+karta25 = Karta("zielony", "lek")
+
 karta26 = Karta("czerwony", "lek")
+karta27 = Karta("czerwony", "lek")
+karta28 = Karta("czerwony", "lek")
+karta29 = Karta("czerwony", "lek")
 
-karta27 = Karta("niebieski", "wirus")
-karta28 = Karta("niebieski", "wirus")
-karta29 = Karta("niebieski", "wirus")
-karta30 = Karta("niebieski", "wirus")
+karta30 = Karta("niebieski", "lek")
+karta31 = Karta("niebieski", "lek")
+karta32 = Karta("niebieski", "lek")
+karta33 = Karta("niebieski", "lek")
 
-karta31 = Karta("niebieski", "organ")
-karta32 = Karta("niebieski", "organ")
-karta33 = Karta("niebieski", "organ")
-karta34 = Karta("niebieski", "organ")
-karta35 = Karta("niebieski", "organ")
+karta34 = Karta("żółty", "lek")
+karta35 = Karta("żółty", "lek")
+karta36 = Karta("żółty", "lek")
+karta37 = Karta("żółty", "lek")
 
-karta36 = Karta("niebieski", "lek")
-karta37 = Karta("niebieski", "lek")
-karta38 = Karta("niebieski", "lek")
-karta39 = Karta("niebieski", "lek")
+karta38 = Karta("wielokolorowy", "lek")
+karta39 = Karta("wielokolorowy", "lek")
+karta40= Karta("wielokolorowy", "lek")
+karta41 = Karta("wielokolorowy", "lek")
 
-karta40 = Karta("żółty", "wirus")
-karta41 = Karta("żółty", "wirus")
-karta42 = Karta("żółty", "wirus")
-karta43 = Karta("żółty", "wirus")
+karta42 = Karta("zielony", "wirus")
+karta43 = Karta("zielony", "wirus")
+karta44 = Karta("zielony", "wirus")
+karta45 = Karta("zielony", "wirus")
 
-karta44 = Karta("żółty", "organ")
-karta45 = Karta("żółty", "organ")
-karta46 = Karta("żółty", "organ")
-karta47 = Karta("żółty", "organ")
-karta48 = Karta("żółty", "organ")
+karta46 = Karta("czerwony", "wirus")
+karta47 = Karta("czerwony", "wirus")
+karta48 = Karta("czerwony", "wirus")
+karta49 = Karta("czerwony", "wirus")
 
-karta49 = Karta("żółty", "lek")
-karta50 = Karta("żółty", "lek")
-karta51 = Karta("żółty", "lek")
-karta52 = Karta("żółty", "lek")
+karta50 = Karta("niebieski", "wirus")
+karta51 = Karta("niebieski", "wirus")
+karta52 = Karta("niebieski", "wirus")
+karta53 = Karta("niebieski", "wirus")
 
-karta53 = Karta("wielokolorowy", "wirus")
+karta54 = Karta("żółty", "wirus")
+karta55 = Karta("żółty", "wirus")
+karta56 = Karta("żółty", "wirus")
+karta57 = Karta("żółty", "wirus")
 
-karta54 = Karta("wielokolorowy", "organ")
-
-karta55 = Karta("wielokolorowy", "lek")
-karta56 = Karta("wielokolorowy", "lek")
-karta57= Karta("wielokolorowy", "lek")
-karta58 = Karta("wielokolorowy", "lek")
-
+karta58 = Karta("wielokolorowy", "wirus")
+'''
 
 #okroić talie tylko do organow
+'''
 talia = [karta1, karta2, karta3, karta4, karta5, karta6, karta7, karta8, karta9, karta10, 
          karta11, karta12, karta13, karta14, karta15, karta16, karta17, karta18, karta19, 
          karta20, karta21, karta22, karta23, karta24, karta25, karta26, karta27, karta28, karta29, 
          karta30, karta31, karta32, karta33, karta34, karta35, karta36, karta37, karta38, karta39, 
          karta40, karta41, karta42, karta43, karta44, karta45, karta46, karta47, karta48, karta49, 
          karta50, karta51, karta52, karta53, karta54, karta55, karta56, karta57, karta58]
+'''
+talia = [karta1, karta2, karta3, karta4, karta5, karta6, karta7, karta8, karta9, karta10, 
+         karta11, karta12, karta13, karta14, karta15, karta16, karta17, karta18, karta19, 
+         karta20, karta21]
 
 
 if __name__ == "__main__":
