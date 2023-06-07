b9c4f711f55b8e7f661574dde435187f9e37a62f
anna-rudenko-pwr <82343960+anna-rudenko-pwr@users.noreply.github.com>
2023-06-08 00:58:46 +0200

    Update plansza.py

--- plansza.py
+++ plansza.py
@@ -3,22 +3,26 @@ from interfejsy import InterfejsPlanszy
 from time import sleep
 from UI import ui
 
+
 class Plansza(InterfejsPlanszy):
 
     def __init__(self, talia):
         self.talia = talia  # lista objektow klasy Karta
         self.karty_pozostale = self.talia[:]
         self.karty_uzyte = []
-        self.ui=ui(self)
+        self.ui = ui(self)
         self.karty_polozone = {}
         self.karty_na_reku = {}
+        self.ilosc_graczy = 0
+        self.rachunek = {}
 
     def rozdaj_karty(self, ilosc_graczy):
+        self.ilosc_graczy = ilosc_graczy
         for gracz_id in range(1, ilosc_graczy + 1):
             self.karty_polozone[gracz_id] = []
             self.karty_na_reku[gracz_id] = []
             self.daj_karty_graczowi(3, gracz_id)
-    
+
     '''def daj_karty_graczowi(self, ilosc_kart, gracz_id):
         rozdane_karty = 0
         while rozdane_karty != ilosc_kart:
@@ -33,7 +37,7 @@ class Plansza(InterfejsPlanszy):
             if not self.karty_pozostale:
                 self.tasuj_karty(self.karty_uzyte, self.karty_pozostale)
             karta = random.choice(self.karty_pozostale)
-            print(karta)
+
             if type(karta) is list:
                 print(self.karty_pozostale)
                 for i in karta:
@@ -43,7 +47,7 @@ class Plansza(InterfejsPlanszy):
                 print(karta.kolor, karta.funkcja)
 
             self.karty_na_reku[gracz_id].append(karta)
-            print(self.karty_na_reku[gracz_id])
+
             self.karty_pozostale.remove(karta)
             rozdane_karty += 1
 
@@ -51,20 +55,22 @@ class Plansza(InterfejsPlanszy):
         if karta in self.karty_na_reku[gracz_id]:
             wylozone_karty_gracza = self.karty_polozone[gracz_id_wyloz]
             if karta.funkcja == 'lek':
-                leki_tego_samego_koloru = [i for i in wylozone_karty_gracza if i.funkcja == 'lek' and i.kolor == karta.kolor]
+                leki_tego_samego_koloru = [
+                    i for i in wylozone_karty_gracza if i.funkcja == 'lek' and i.kolor == karta.kolor]
                 if len(leki_tego_samego_koloru) >= 2:
                     return False
                 for i in wylozone_karty_gracza:
                     if i.funkcja == 'organ' and i.kolor == karta.kolor:
                         if wylozone_karty_gracza.count(karta) < 2:
-                            return True   
+                            return True
             elif karta.funkcja == 'organ':
                 for i in wylozone_karty_gracza:
                     if i.funkcja == 'organ' and i.kolor == karta.kolor:
                         return False
                 return True
             elif karta.funkcja == 'wirus':
-                leki_tego_samego_koloru = [i for i in wylozone_karty_gracza if i.funkcja == 'lek' and i.kolor == karta.kolor]
+                leki_tego_samego_koloru = [
+                    i for i in wylozone_karty_gracza if i.funkcja == 'lek' and i.kolor == karta.kolor]
                 if len(leki_tego_samego_koloru) >= 2:
                     return False
                 for i in wylozone_karty_gracza:
@@ -91,31 +97,34 @@ class Plansza(InterfejsPlanszy):
         # 2 leki 1 wirus - niemozliwe, sprawdzane podczas walidacji ruchu
         if len(wirusy) == 2:
             karty_do_usuniecia = wirusy + [organ]
-            self.karty_polozone[gracz_id_wyloz] = [i for i in self.karty_polozone[gracz_id_wyloz] if i not in karty_do_usuniecia]
+            self.karty_polozone[gracz_id_wyloz] = [
+                i for i in self.karty_polozone[gracz_id_wyloz] if i not in karty_do_usuniecia]
             self.karty_uzyte += karty_do_usuniecia
         elif len(wirusy) == 1 and len(leki) == 1:
             karty_do_usuniecia = leki + wirusy
-            self.karty_polozone[gracz_id_wyloz] = [i for i in self.karty_polozone[gracz_id_wyloz] if i not in karty_do_usuniecia]
+            self.karty_polozone[gracz_id_wyloz] = [
+                i for i in self.karty_polozone[gracz_id_wyloz] if i not in karty_do_usuniecia]
             self.karty_uzyte += karty_do_usuniecia
 
     def poloz_karte(self, indeks_karty, gracz_id, gracz_id_wyloz):
         karta = self.karty_na_reku[gracz_id][indeks_karty]
         self.karty_polozone[gracz_id_wyloz].append(karta)
         self.karty_na_reku[gracz_id].remove(karta)
-    
+
     def wymien_karte(self, indeks_karty, gracz_id):
-        #print('wymieniam karte '+ self.karty_na_reku[gracz_id][indeks_karty].kolor + self.karty_na_reku[gracz_id][indeks_karty].funkcja)
+        # print('wymieniam karte '+ self.karty_na_reku[gracz_id][indeks_karty].kolor + self.karty_na_reku[gracz_id][indeks_karty].funkcja)
         self.karty_uzyte.append(self.karty_na_reku[gracz_id][indeks_karty])
-        self.karty_na_reku[gracz_id].remove(self.karty_na_reku[gracz_id][indeks_karty])
+        self.karty_na_reku[gracz_id].remove(
+            self.karty_na_reku[gracz_id][indeks_karty])
 
     def ruch_gracza(self, gracz_id):
-        #UI.pokaz_karty(gracz_id)
-    # self.ui.pokaz_karty_gracza(gracz_id)
+        # UI.pokaz_karty(gracz_id)
+        # self.ui.pokaz_karty_gracza(gracz_id)
         pass
 
     def poinformuj_graczy_o_ruchu(self, delta):
-        #delta = {'akcja': wyloz/wyien, 'indeks_karty': indeks_karty wykladanej/wymienianej, 'gracz_id' : gracz_id osoby wykonujacej ruch,
-            #  'gracz_id_wyloz': gracz_id u kogo sie wyklada karte }
+        # delta = {'akcja': wyloz/wyien, 'indeks_karty': indeks_karty wykladanej/wymienianej, 'gracz_id' : gracz_id osoby wykonujacej ruch,
+        #  'gracz_id_wyloz': gracz_id u kogo sie wyklada karte }
         if delta['akcja'] == 'wymien':
             karta = self.karty_uzyte[0].kolor, self.karty_uzyte[0].funkcja
             for gracz in self.gracze:
@@ -128,49 +137,53 @@ class Plansza(InterfejsPlanszy):
     def tasuj_karty(self, ilosc_kart, gracz_id):
         self.karty_pozostale = self.karty_uzyte[:-1]
         self.karty_uzyte = [self.karty_uzyte[-1]]
-        #random.shuffle(karty_tasowane)
-        
-        #input()
+        # random.shuffle(karty_tasowane)
+
+        # input()
 
     def sprawdz_czy_koniec(self):
-        #4 ograny bez zadnego wirusa - stan na 23.05 4 kolory organow - nie moze byc zadnego wirusa
+        # 4 ograny bez zadnego wirusa - stan na 23.05 4 kolory organow - nie moze byc zadnego wirusa
         for gracz_id, karty_gracza in self.karty_polozone.items():
-            karty_z_organem = [karta for karta in karty_gracza if karta.funkcja == "organ"]
+            karty_z_organem = [
+                karta for karta in karty_gracza if karta.funkcja == "organ"]
             if len(karty_z_organem) == 4:
-                wirusy = [karta for karta in karty_gracza if karta.funkcja == "wirus"]
+                wirusy = [
+                    karta for karta in karty_gracza if karta.funkcja == "wirus"]
                 if len(wirusy) == 0:
-                    #print(f"\nKoniec gry! Gracz {gracz_id} ma 4 karty z organem.")
-                    #quit()
-                    #self.tasuj_karty(self, gracz_id)
+                    # print(f"\nKoniec gry! Gracz {gracz_id} ma 4 karty z organem.")
+                    # quit()
+                    # self.tasuj_karty(self, gracz_id)
                     return gracz_id
         return False
 
-
     def rozgrywka(self):
-        #plansza powinna miec liste graczy/botow
+        # plansza powinna miec liste graczy/botow
         koniec = False
         while koniec is False:
             for gracz in self.gracze:
                 print('RUCH GRACZA: ', gracz.gracz_id)
-                #sleep(0.1)
+                # sleep(0.1)
                 print('Karty na rece: ')
                 self.ui.pokaz_karty_na_reku(gracz.gracz_id)
-                #sleep(0.1)
-                print('\nKarty wylozone prez gracza ' + str(gracz.gracz_id) + ': ')
+                # sleep(0.1)
+                print('\nKarty wylozone prez gracza ' +
+                      str(gracz.gracz_id) + ': ')
                 self.ui.pokaz_karty_wylozone_gracza(gracz.gracz_id)
-                #sleep(0.1)
+                # sleep(0.1)
                 print('\nKarty wylozone: ')
                 self.ui.pokaz_wylozone_karty()
-                #sleep(0.1)
+                # sleep(0.1)
                 gracz.wykonaj_ruch()
                 wygrany = self.sprawdz_czy_koniec()
                 if wygrany:
-                    #print("Gracz", wygrany, "wygrał!")
+                    self.ui.pokaz_rezultat_gry(
+                        self.ilosc_graczy, gracz.gracz_id)
                     koniec = True
                     break
-        
 
+                # input('\nNacisnij dowolny przycisk aby kontynuowac: ')
 
-                #input('\nNacisnij dowolny przycisk aby kontynuowac: ')
+    def inizilizacja_rachuku(self):
 
-
+        for i in range(self.ilosc_graczy):
+            self.rachunek["Gracz-" + str(i+1)] = 0
